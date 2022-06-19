d1eee3b0ac0561b206f6543bed9bd8dfe72f0c8c
Anatol Derbisz <anatholius@gmail.com>
2022-06-19 20:30:41 +0200

    `Python libraries`/`Regular expressions`::`Match object and flags`: Filter the results ✅

--- Topics/Match object and flags/Filter the results/main.py
+++ Topics/Match object and flags/Filter the results/main.py
@@ -1,7 +1,6 @@
 import re
 
-
-# put your regex in the variable template
-template = r"Scaramouch."
+template = "Scaramouch."
 string = input()
 match = re.match(template, string)
+print(match and "Match" or "No match")
