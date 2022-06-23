d4605f1613488b866de4f17b8701bad2907b8081
Anatol Derbisz <anatholius@gmail.com>
2022-06-23 03:43:52 +0200

    `Data science`/`NLP`::`Overview of NLTK`: Gutenberg corpus ✅

--- Topics/Overview of NLTK/Gutenberg corpus/main.py
+++ Topics/Overview of NLTK/Gutenberg corpus/main.py
@@ -1,3 +1,10 @@
-# Don't download gutenberg corpus. Just import it
+# 1° Don't download gutenberg corpus. Just import it
+from nltk.corpus import gutenberg
 
-number = int(input()) # a row number of a sentence in Gutenberg corpus
+number = int(input())  # a row number of a sentence in Gutenberg corpus
+
+# 2° get needed sentences
+sentences = gutenberg.sents('whitman-leaves.txt')  # you yourself are `misspelt` :P
+
+# 3° print required sentence
+print(sentences[number])
