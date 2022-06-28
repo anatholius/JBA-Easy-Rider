da760ba85051d05afc5ac63e629987b935adabf3
Anatol Derbisz <anatholius@gmail.com>
2022-06-28 10:52:27 +0200

    `Control flow`/`Iterators and generators`::`Custom generators`: Squares ✅ even better 😃

--- Topics/Custom generators/Squares/main.py
+++ Topics/Custom generators/Squares/main.py
@@ -2,7 +2,7 @@ n = int(input())
 
 
 def squares():
-    yield [str(i ** 2) for i in range(1, n + 1)]
+    yield [i ** 2 for i in range(1, n + 1)]
 
 
-print('\n'.join(next(squares())))
+print(*next(squares()), sep='\n')
